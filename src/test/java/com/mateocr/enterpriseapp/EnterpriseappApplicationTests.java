890af988dd797af475ee3daed17599e31c6a2ad0
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-06-09 08:44:12 -0500

    Inizialized spring project

---
+++ src/test/java/com/mateocr/enterpriseapp/EnterpriseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateocr.enterpriseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
