db6a3ce980d227055aa1d2b218d9b1e8cc8c61fe
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-06-12 16:07:31 -0500

    Fixed swagger erros by deleting springfox dependencies

--- src/main/java/com/mateocr/enterpriseapp/EnterpriseappApplication.java
+++ src/main/java/com/mateocr/enterpriseapp/EnterpriseappApplication.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(
@@ -13,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
         version = "1.0.0",
         description = "Hardware Store API v1.0.0"))
 @EnableReactiveMongoRepositories
-@EnableSwagger2
 public class EnterpriseappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/mateocr/enterpriseapp/config/WebFluxConfig.java
+++ src/main/java/com/mateocr/enterpriseapp/config/WebFluxConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
-public class WebFluxConfig implements WebFluxConfigurer {
-
+public class WebFluxConfig {
     @Bean
     public WebFluxConfigurer corsConfigure() {
         return new WebFluxConfigurerComposite() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*")
+                registry.addMapping("/*").allowedOrigins("*")
                         .allowedMethods("*");
             }
         };
