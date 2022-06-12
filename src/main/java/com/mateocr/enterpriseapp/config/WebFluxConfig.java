d49827de10ee78435fecffab2b3ee175c837016c
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-06-12 16:16:52 -0500

    Fixing cors issues on webflux config class

--- src/main/java/com/mateocr/enterpriseapp/config/WebFluxConfig.java
+++ src/main/java/com/mateocr/enterpriseapp/config/WebFluxConfig.java
@@ -14,7 +14,7 @@ public class WebFluxConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("*")
+                registry.addMapping("/**").allowedOrigins("*")
                         .allowedMethods("*");
             }
         };
