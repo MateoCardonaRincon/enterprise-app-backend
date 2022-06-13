b7d45f7af86ee1f0c17783b6d955b7490dac6d18
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-06-13 12:10:47 -0500

    N-th try of fixing the CORS origin problem

--- src/main/java/com/mateocr/enterpriseapp/config/WebFluxConfig.java
+++ src/main/java/com/mateocr/enterpriseapp/config/WebFluxConfig.java
@@ -3,10 +3,12 @@ package com.mateocr.enterpriseapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
+@EnableWebFlux
 public class WebFluxConfig {
     @Bean
     public WebFluxConfigurer corsConfigure() {
@@ -14,8 +16,9 @@ public class WebFluxConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*")
-                        .allowedMethods("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // 0.0.0.0/0
+                        .allowedMethods("*"); // GET, POST, PUT, DELETE, ETC...
             }
         };
     }
